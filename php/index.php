b567299e7ebbd26cbf33504469210d0d2c6b3013
David Brown <davembrownx@gmail,com>
2023-10-29 14:54:10 -0400

    Twelfth time's a charm

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <html>
   <body style="background-color:black">
     <h1 style="color:Tomato">Say Hi To Dave</h1>
-      <p style="color;DodgerBlue">
+      <p style="color:DodgerBlue">
         <?php echo "Lend me five dollars Dave!" ?>
       </p>
   </body>
