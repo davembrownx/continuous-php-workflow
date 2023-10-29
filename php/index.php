4d985767532bcabc3910f5a41abdb3f560ae8c5d
David Brown <davembrownx@gmail,com>
2023-10-29 14:51:08 -0400

    Hopefully fixing colors

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <html>
   <body style="background-color:black">
     <h1 style="color:Tomato">Say Hi To Dave</h1>
-      <p style="color;Dodgerblue">
+      <p style="color;DodgerBlue">
         <?php echo "Lend me five dollars Dave!" ?>
       </p>
   </body>
